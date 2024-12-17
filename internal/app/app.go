ed55c68536e8ff388840dd5659785850c251bfde
BenjaminArmijo3 <43712424+BenjaminArmijo3@users.noreply.github.com>
2024-12-17 13:33:46 -0300

    first

---
+++ internal/app/app.go
@@ -0,0 +1,29 @@
+package app
+
+import (
+	"log/slog"
+	grpcapp "sso/internal/app/grpc"
+	"sso/internal/services/auth"
+	"sso/internal/storage/sqlite"
+	"time"
+)
+
+type App struct {
+	GRPCServer *grpcapp.App
+}
+
+func New(log *slog.Logger, grpcport int, storagePath string, tokenTTL time.Duration) *App {
+	storage, err := sqlite.New(storagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	authService := auth.New(log, storage, storage, storage, tokenTTL)
+
+	grpcApp := grpcapp.New(log, authService, grpcport)
+
+	return &App{
+		GRPCServer: grpcApp,
+	}
+
+}
